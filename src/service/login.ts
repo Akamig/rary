8cffad25e99e45d7efa44f6a487e91e9e40f023f
Akamig <akamig@posteo.net>
2020-07-18 03:50:25 +0900

    Login req with error handling

--- src/service/login.ts
+++ src/service/login.ts
@@ -5,4 +5,23 @@ export default async function login(id: string, password: string) {
     user_id: id,
     user_password: password,
   });
+
+  const LoginRes = await fetch(`${ssoPrefix}${url}Login.do`, {
+    method: 'POST',
+    body: `${LoginForm}`,
+    redirect: 'manual',
+  }).then(async (response) => {
+    let redirectUrl = response.headers.get('location');
+    if (response.status !== 302) {
+      throw new Error(`Response: ${response.status}, Login redirection failed.`);
+    }
+    if (redirectUrl === null) {
+      throw new Error('Login failed, No redirection URL found.');
+    }
+    if (redirectUrl?.includes('portal')) {
+      throw new Error('Login failed. Your account seems locked.');
+    }
+    return redirectUrl;
+  });
+
 }
