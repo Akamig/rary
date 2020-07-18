82d11e30a1b10149a86e9d7dc34f9a0405b672b9
Akamig <akamig@posteo.net>
2020-07-18 11:06:09 +0900

    client constructor gets tacocat

--- src/model/Client.ts
+++ src/model/Client.ts
@@ -1,5 +1,13 @@
 import * as service from '../service';
+
+import Tacocat from './Tacocat';
+
 class Client {
+  tacocat: Tacocat;
+
+  constructor(tacocat: Tacocat) {
+    this.tacocat = tacocat;
+  }
 }
 
 export default Client;
