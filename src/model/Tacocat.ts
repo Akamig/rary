8133093911f8bedf6bdb10575a50b96d0fa3fa4d
Akamig <akamig@posteo.net>
2020-07-21 20:29:18 +0900

    Added tacocat constructor

--- src/model/Tacocat.ts
+++ src/model/Tacocat.ts
@@ -1,13 +1,25 @@
 /*
-** Environment Variables interface, named after this tweet.
-** https://twitter.com/_Akamig/status/1283437475663212545
-*/
+ ** Environment Variables interface, named after this tweet.
+ ** https://twitter.com/_Akamig/status/1283437475663212545
+ */
 
-interface Tacocat {
-  SSOURL: string;
-  LIBURL: string;
-  SSOLOGON: string;
-  L_TOKEN: string;
+class Tacocat {
+  public SSOURL: string;
+  public LIBURL: string;
+  public SSOLOGON: string;
+  public L_TOKEN: string;
+
+  constructor(
+    SSOURL: string,
+    LIBURL: string,
+    SSOLOGON: string,
+    L_TOKEN: string
+  ) {
+    this.SSOURL = SSOURL;
+    this.LIBURL = LIBURL;
+    this.SSOLOGON = SSOLOGON;
+    this.L_TOKEN = L_TOKEN;
+  }
 }
 
 export { Tacocat };
