4c4691b9741d4aa7f4c213a19ca7a81a27c375a8
Akamig <Akamig@users.noreply.github.com>
2020-07-23 03:45:19 +0900

    dueRenew implemented

--- src/model/Client.ts
+++ src/model/Client.ts
@@ -14,6 +14,12 @@ class Client {
     this.cookie = await service.login(id, password, this.tacocat);
   }
 
+
+  async dueRenew(book: Book) {
+    return service.dueRenew(book, this.cookie, this.tacocat);
+  }
+  
+  
   async getBookList() {
     return service.getBookList(this.cookie, this.tacocat);
   }
